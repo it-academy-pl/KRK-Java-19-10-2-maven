6768f110f646e8bd6ceed71ddd828a291f17a019
itacademy <itacademykrakow@gmail.com>
2019-12-23 18:33:41 +0100

    maven demo sources

---
+++ src/main/java/pl/itacademy/maven/HelloWorld.java
@@ -0,0 +1,8 @@
+package pl.itacademy.maven;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven world!");
+        System.out.println(new Person("Jan", "Kowalski"));
+    }
+}
---
+++ src/main/java/pl/itacademy/maven/Person.java
@@ -0,0 +1,9 @@
+package pl.itacademy.maven;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private final String firstName;
+    private final String lastName;
+}
